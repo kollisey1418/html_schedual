ef49985f0444fb527a8da8d1d2d420e7e8862ccc
kollisey1418 <kollisey1418@gmail.com>
2025-03-23 13:39:46 +0100

    Добавил ветер в погоду

--- weather.js
+++ weather.js
@@ -3,10 +3,15 @@ async function getWeather(city, elementId = "weather", lang = "en") {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&lang=${lang}&aqi=no`);
     const data = await response.json();
 
+    let windUnit;
+    if (lang === "ru") windUnit = "км/ч";
+    else if (lang === "sr") windUnit = "km/h";
+    else windUnit = "kph";
+
     const container = document.getElementById(elementId);
     if (container) {
         container.innerHTML = `
-            <p><strong>${data.location.name}</strong>: ${data.current.temp_c}°C, ${data.current.condition.text}</p>
+            <p><strong>${data.location.name}</strong>: ${data.current.temp_c}°C, ${data.current.condition.text}, ${data.current.wind_kph} ${windUnit}</p>
             <img src="https:${data.current.condition.icon}" alt="Weather Icon">
         `;
     }
