28c07bdb76b32115e1b748cd40daf1e9eaf85c98
kollisey1418 <kollisey1418@gmail.com>
2025-06-05 20:47:22 +0200

    1. Веточное отображение расписания для мобильных устройств 2. Сортировка для мобильных усьройств

--- js/togglePrice.js
+++ js/togglePrice.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const toggleBtn = document.getElementById('togglePrice');
-  const priceBlock = document.getElementById('priceSection');
+  if (window.innerWidth <= 550) {
+    const toggleBtn = document.getElementById('togglePrice');
+    const priceBlock = document.getElementById('priceSection');
 
-  if (toggleBtn && priceBlock) {
-    toggleBtn.addEventListener('click', () => {
-      const isHidden = priceBlock.style.display === 'none' || getComputedStyle(priceBlock).display === 'none';
+    if (toggleBtn && priceBlock) {
+      toggleBtn.addEventListener('click', () => {
+        const isHidden = priceBlock.style.display === 'none' || getComputedStyle(priceBlock).display === 'none';
 
-      priceBlock.style.display = isHidden ? 'flex' : 'none';
-      toggleBtn.src = isHidden ? 'img/toggle_up.png' : 'img/toggle_down.png';
-    });
+        priceBlock.style.display = isHidden ? 'flex' : 'none';
+        toggleBtn.src = isHidden ? 'img/toggle_up.png' : 'img/toggle_down.png';
+      });
+    }
   }
 });
